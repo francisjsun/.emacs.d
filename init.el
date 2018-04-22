f80f863373a65fda2dac224703d0786e2587c103
Francis Sun <francisjsun@outlook.com>
2018-04-22 18:55:07 +0800

    rainbrow-delimiters color issue fixed

--- init.el
+++ init.el
@@ -113,17 +113,3 @@ Argument CMD shell cmd"
  '(package-selected-packages
    (quote
     (glsl-mode lua-mode company-c-headers company flycheck rainbow-delimiters ace-window smartparens markdown-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(rainbow-delimiters-depth-1-face ((t (:foreground "blue"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "white"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "green"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "blue"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "white"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "green"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "blue"))))
- '(rainbow-delimiters-mismatched-face ((t (:foreground "red"))))
- '(rainbow-delimiters-unmatched-face ((t (:foreground "red")))))
